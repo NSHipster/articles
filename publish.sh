d391025d1cf3f4a143ff5a265b4df49791c999f3
Mattt Thompson <m@mattt.me>
2013-12-17 10:25:15 -0800

    Adding HTML tidy to publishing workflow

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 jekyll build --config _config.en.yml
+find _site/ -iname "*.html" -exec tidy -config tidy.conf {} +
 find _site/ -iname '*.html' -exec gzip -n --best {} +
 find _site/ -iname '*.xml' -exec gzip -n --best {} +
 
@@ -7,3 +8,11 @@ for f in `find _site/ -iname '*.gz'`; do
 done
 
 s3cmd sync --progress -M --acl-public --add-header 'Content-Encoding:gzip' _site/ s3://nshipster.com/ --exclude '*.*' --include '*.html' --include '*.xml' --verbose
+
+# compass compile assets
+# find assets/ -iname '*.css' -exec gzip -n --best {} +
+# find assets/ -iname '*.svg' -exec gzip -n --best {} +
+# for f in `find assets/ -iname '*.gz'`; do
+#   mv $f ${f%.gz}
+# done
+# s3cmd sync --progress -M --acl-public assets/ s3://cdn.nshipster.com/ --exclude '*.*' --include '*.css' --include '*.svg' --no-delete-removed --verbose
