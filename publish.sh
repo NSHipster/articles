55a9db3df354f49afe25bd2e7edc1ea85840e748
Mattt Thompson <m@mattt.me>
2014-03-07 12:57:51 -0800

    Updating publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 jekyll build --config _config.en.yml
 find _site/ -iname "*.html" -exec tidy -config tidy.conf {} +
 find _site/ -iname '*.html' -exec gzip -n --best {} +
@@ -9,10 +11,10 @@ done
 
 s3cmd sync --progress -M --acl-public --add-header 'Content-Encoding:gzip' _site/ s3://nshipster.com/ --exclude '*.*' --include '*.html' --include '*.xml' --verbose
 
-# compass compile assets
-# find assets/ -iname '*.css' -exec gzip -n --best {} +
-# find assets/ -iname '*.svg' -exec gzip -n --best {} +
-# for f in `find assets/ -iname '*.gz'`; do
-#   mv $f ${f%.gz}
-# done
-# s3cmd sync --progress -M --acl-public assets/ s3://cdn.nshipster.com/ --exclude '*.*' --include '*.css' --include '*.svg' --no-delete-removed --verbose
+compass compile assets --force
+find assets/css -iname '*.css' -exec gzip -n --best {} +
+for f in `find assets/css -iname '*.gz'`; do
+  mv $f ${f%.gz}
+done
+
+s3cmd put --recursive --progress -M --acl-public --add-header 'Content-Encoding:gzip' assets/css s3://nshipster.com/
