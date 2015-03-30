4d292367759297037376a86eb8ef27a554dc891c
Nate Cook <natecook@gmail.com>
2015-03-30 16:58:39 -0400

    Remember Swift/ObjC language choice

--- assets/js/application.js
+++ assets/js/application.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
   });
   
   var hiddenSingleLanguages = $("meta[property='nshipster:hide-single-lang']").attr('content').split(',');
+  
   $('.highlight-group').each(function() {
     var languages = [];
     $(this).find($("code")).each(function() {
@@ -41,8 +42,8 @@ $(document).ready(function() {
     $(this).prepend(span);
   });
 
-  $('a[data-lang]').on('click', function() {
-    var lang = $(this).data('lang');
+
+  var showAllWithLanguage = function(lang) {
     $('a[data-lang=' + lang + ']').each( function() {
       $(this).siblings('a').removeClass('active');
       $(this).addClass('active');
@@ -54,7 +55,20 @@ $(document).ready(function() {
         }
       });
     });
+  };
+
+  $('a[data-lang]').on('click', function() {
+    var lang = $(this).data('lang');
+    if (['swift', 'objective-c'].indexOf(lang) != -1) {
+      $.fn.cookie('pref-lang', lang, { expires : 3650, path: '/' });
+    }
+    showAllWithLanguage(lang);
   });
+  
+  var preferredLanguage = $.fn.cookie('pref-lang');
+  if (preferredLanguage) {
+    showAllWithLanguage(preferredLanguage);
+  }
 });
 
 String.prototype.toProperCase = function() {
@@ -64,3 +78,44 @@ String.prototype.toProperCase = function() {
     default: return this.replace(/\b\w+/g, function(txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
   }
 };
+
+// Zepto.cookie plugin
+// 
+// Copyright (c) 2010, 2012 
+// @author Klaus Hartl (stilbuero.de)
+// @author Daniel Lacy (daniellacy.com)
+// 
+// Dual licensed under the MIT and GPL licenses:
+// http://www.opensource.org/licenses/mit-license.php
+// http://www.gnu.org/licenses/gpl.html
+;(function($){
+    $.extend($.fn, {
+cookie : function (key, value, options) {
+var days, time, result, decode
+// A key and value were given. Set cookie.
+if (arguments.length > 1 && String(value) !== "[object Object]") {
+// Enforce object
+                options = $.extend({}, options)
+if (value === null || value === undefined) options.expires = -1
+if (typeof options.expires === 'number') {
+                    days = (options.expires * 24 * 60 * 60 * 1000)
+                    time = options.expires = new Date()
+                    time.setTime(time.getTime() + days)
+                }
+                value = String(value)
+return (document.cookie = [
+                    encodeURIComponent(key), '=',
+                    options.raw ? value : encodeURIComponent(value),
+                    options.expires ? '; expires=' + options.expires.toUTCString() : '',
+                    options.path ? '; path=' + options.path : '',
+                    options.domain ? '; domain=' + options.domain : '',
+                    options.secure ? '; secure' : ''
+                ].join(''))
+            }
+// Key and possibly options given, get cookie
+            options = value || {}
+            decode = options.raw ? function (s) { return s } : decodeURIComponent
+return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null
+        }
+    })
+})(Zepto)
